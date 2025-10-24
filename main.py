4889084db3aa5ab7bbd2d36394e8f11692ebc5fd
umairasdev <umairashrafbsse@gmail.com>
2025-10-24 23:52:34 +0500

    added cors middleware

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from route import router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from route import router
 
 app = FastAPI()
 
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
+app.include_router(router)
 
 @app.get("/")
 def read_root():
